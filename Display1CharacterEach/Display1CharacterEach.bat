40f67b252481634f90a647cfba8044276084547e
Project-S-31415 <projects31415.gif@gmail.com>
2023-01-23 22:15:18 +0900

    遅延スキップ文字を変更可能に　引数指定時すぐ表示できるように　文修正

---
+++ Display1CharacterEach/Display1CharacterEach.bat
@@ -0,0 +1,3 @@
+﻿rem コマンドプロンプトの文字色を緑にして
+color 02
+Display1CharacterEach.exe 10 t
--- Display1CharacterEach/Program.cs
+++ Display1CharacterEach/Program.cs
@@ -14,18 +14,24 @@ namespace Display1CharacterEach
             Console.WriteLine("Setting");
             Console.WriteLine("1.Text setting");
             if (!File.Exists("DisplayText.txt"))
-                File.WriteAllText("DisplayText.txt", Resources.SampleText);
-            Console.WriteLine("If change text, enter \"t\".");
-            if (Console.ReadLine().Contains("t"))
             {
-                Console.WriteLine("Please write display text in \"DisplayText.txt\".");
-                Process.Start("notepad.exe", "DisplayText.txt");
-                Console.WriteLine("If finish write, press \"Enter\" key.");
-                Console.ReadLine();
+                File.WriteAllText("DisplayText.txt", Resources.SampleText);
+                Console.WriteLine("\"DisplayText.txt\" not found. Sample text written to \"DisplayText.txt\".");
+            }
+            if (args.Count() == 0)//引数指定なし
+            {
+                Console.WriteLine("If change text, enter \"t\".");
+                if (Console.ReadLine().Contains("t"))
+                {
+                    Console.WriteLine("Please write display text in \"DisplayText.txt\".");
+                    Process.Start("notepad.exe", "DisplayText.txt");
+                    Console.WriteLine("If finish writing, press \"Enter\" key.");
+                    Console.ReadLine();
+                }
             }
             string Text = File.ReadAllText("DisplayText.txt");
             int Delay = -1;
-            bool SkipSpace = false;
+            string _SkipChar = "";
             Console.WriteLine("2.Delay setting");
             if (args.Count() >= 1)
                 try
@@ -34,7 +40,7 @@ namespace Display1CharacterEach
                 }
                 catch
                 {
-                    Console.WriteLine("Failed to acquire delay.");
+                    Console.WriteLine("Failed to set delay.");
                 }
             while (Delay < 0)
                 try
@@ -44,28 +50,69 @@ namespace Display1CharacterEach
                 }
                 catch
                 {
-                    Console.WriteLine("Failed to acquire delay. Please try again.");
+                    Console.WriteLine("Failed to set delay. Please try again.");
                 }
-            Console.WriteLine("3.Skip setting");
+            Console.WriteLine("3.Skip delay setting");
             if (args.Count() >= 2)
-                SkipSpace = args[1] == "true";
+                try
+                {
+                    char SkipChar_ = Convert.ToChar(args[1].Remove(0, 1).Remove(1, 1));//1文字である必要
+                    _SkipChar = Convert.ToString(SkipChar_);
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to set skip character.");
+                }
+            while (_SkipChar == "")
+            {
+                try
+                {
+                    Console.WriteLine("Enter one character to skip delay or \"Enter\"");
+                    string SkipChar__ = Console.ReadLine();
+                    if (SkipChar__ == "")
+                        break;
+                    char SkipChar_ = Convert.ToChar(SkipChar__);
+                    _SkipChar = Convert.ToString(SkipChar_);
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to set one character to skip delay. Must be one character or null. Please try again.");
+                }
+            }
+            if (_SkipChar != "")
+            {
+                char SkipChar = Convert.ToChar(_SkipChar);
+                Console.WriteLine("Preparing for display...");
+                if (args.Count() == 0)
+                    Thread.Sleep(1000);
+                while (true)
+                {
+                    string _Text = Text;
+                    Console.Clear();
+                    while (_Text.Length > 0)
+                    {
+                        if (_Text.First() != SkipChar)
+                            Thread.Sleep(Delay);
+                        Console.Write(_Text.First());
+                        _Text = _Text.Remove(0, 1);
+                    }
+                }
+            }
             else
             {
-                Console.WriteLine("If skip the delay when space(\" \"), enter \"t\".");
-                SkipSpace = Console.ReadLine().Contains("t");
-            }
-            Console.WriteLine("Preparing for display...");
-            Thread.Sleep(1000);
-            while (true)
-            {
-                string _Text = Text;
-                Console.Clear();
-                while (_Text.Length > 0)
+                Console.WriteLine("Preparing for display...");
+                if (args.Count() == 0)
+                    Thread.Sleep(1000);
+                while (true)
                 {
-                    if (SkipSpace == false || _Text.First() != ' ')
+                    string _Text = Text;
+                    Console.Clear();
+                    while (_Text.Length > 0)
+                    {
                         Thread.Sleep(Delay);
-                    Console.Write(_Text.First());
-                    _Text = _Text.Remove(0, 1);
+                        Console.Write(_Text.First());
+                        _Text = _Text.Remove(0, 1);
+                    }
                 }
             }
         }
