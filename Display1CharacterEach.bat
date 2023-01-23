a63fcdf5c70763caa4b7a25c6d1ce5dcced74625
Ichihai1415 <98251808+Ichihai1415@users.noreply.github.com>
2023-01-23 23:00:04 +0900

    v1.0.1 遅延スキップ文字を変更可能に　引数指定時すぐ表示できるように　各文章修正

---
+++ Display1CharacterEach.bat
@@ -0,0 +1,5 @@
+@echo off
+rem コマンドプロンプトの文字色を緑にして表示
+color 0A
+Display1CharacterEach.exe 10 space
+rem 詳細はREADME.md参照
--- Program.cs
+++ Program.cs
@@ -11,21 +11,29 @@ namespace Display1CharacterEach
     {
         static void Main(string[] args)
         {
+            if (args.Length >= 2)
+                args[1]=args[1].Replace("space", " ");
             Console.WriteLine("Setting");
             Console.WriteLine("1.Text setting");
             if (!File.Exists("DisplayText.txt"))
-                File.WriteAllText("DisplayText.txt", Resources.SampleText);
-            Console.WriteLine("If change text, enter \"t\".");
-            if (Console.ReadLine().Contains("t"))
             {
-                Console.WriteLine("Please write display text in \"DisplayText.txt\".");
-                Process.Start("notepad.exe", "DisplayText.txt");
-                Console.WriteLine("If finish write, press \"Enter\" key.");
-                Console.ReadLine();
+                File.WriteAllText("DisplayText.txt", Resources.SampleText);
+                Console.WriteLine("\"DisplayText.txt\" not found. Sample text written to \"DisplayText.txt\".");
+            }
+            if (args.Count() == 0)//引数指定なし
+            {
+                Console.WriteLine("If change text, enter \"t\".");
+                if (Console.ReadLine().Contains("t"))
+                {
+                    Console.WriteLine("Please write display text in \"DisplayText.txt\".");
+                    Process.Start("notepad.exe", "DisplayText.txt");
+                    Console.WriteLine("If finish writing, press \"Enter\" key.");
+                    Console.ReadLine();
+                }
             }
             string Text = File.ReadAllText("DisplayText.txt");
             int Delay = -1;
-            bool SkipSpace = false;
+            string SkipChar_ = "";
             Console.WriteLine("2.Delay setting");
             if (args.Count() >= 1)
                 try
@@ -34,7 +42,7 @@ namespace Display1CharacterEach
                 }
                 catch
                 {
-                    Console.WriteLine("Failed to acquire delay.");
+                    Console.WriteLine("Failed to set delay.");
                 }
             while (Delay < 0)
                 try
@@ -44,28 +52,72 @@ namespace Display1CharacterEach
                 }
                 catch
                 {
-                    Console.WriteLine("Failed to acquire delay. Please try again.");
+                    Console.WriteLine("Failed to set delay. Please try again.");
                 }
-            Console.WriteLine("3.Skip setting");
-            if (args.Count() >= 2)
-                SkipSpace = args[1] == "true";
+            Console.WriteLine("3.Skip delay setting");
+            try//if (args.Count() >= 2) 無理やり処理統一
+            {
+                char SkipChar__ = Convert.ToChar(args[1]);//1文字である必要
+                SkipChar_ = Convert.ToString(SkipChar__);
+            }
+            catch
+            {
+                if (args.Count() >= 2)
+                    if (args[1] != "null")
+                        Console.WriteLine("Failed to set skip character.");
+                while (SkipChar_ == "")
+                    try
+                    {
+                        if (args.Count() >= 2)
+                            if (args[1] == "null")
+                                break;
+                        Console.WriteLine("Enter one character to skip delay or \"Enter\"");
+                        string SkipChar__ = Console.ReadLine();
+                        if (SkipChar__ == "")
+                            break;
+                        char SkipChar___ = Convert.ToChar(SkipChar__);
+                        SkipChar_ = Convert.ToString(SkipChar___);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Failed to set one character to skip delay. Must be one character or null. Please try again.");
+                    }
+            }
+
+            if (SkipChar_ != "")
+            {
+                char SkipChar = Convert.ToChar(SkipChar_);
+                Console.WriteLine("Preparing for display...");
+                if (args.Count() == 0)
+                    Thread.Sleep(1000);
+                while (true)
+                {
+                    string _Text = Text;
+                    Console.Clear();
+                    while (_Text.Length > 0)
+                    {
+                        if (_Text.First() != SkipChar)
+                            Thread.Sleep(Delay);
+                        Console.Write(_Text.First());
+                        _Text = _Text.Remove(0, 1);
+                    }
+                }
+            }
             else
             {
-                Console.WriteLine("If skip the delay when space(\" \"), enter \"t\".");
-                SkipSpace = Console.ReadLine().Contains("t");
-            }
-            Console.WriteLine("Preparing for display...");
-            Thread.Sleep(1000);
-            while (true)
-            {
-                string _Text = Text;
-                Console.Clear();
-                while (_Text.Length > 0)
+                Console.WriteLine("Preparing for display...");
+                if (args.Count() == 0)
+                    Thread.Sleep(1000);
+                while (true)
                 {
-                    if (SkipSpace == false || _Text.First() != ' ')
+                    string _Text = Text;
+                    Console.Clear();
+                    while (_Text.Length > 0)
+                    {
                         Thread.Sleep(Delay);
-                    Console.Write(_Text.First());
-                    _Text = _Text.Remove(0, 1);
+                        Console.Write(_Text.First());
+                        _Text = _Text.Remove(0, 1);
+                    }
                 }
             }
         }
